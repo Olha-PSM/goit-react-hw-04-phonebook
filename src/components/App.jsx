f1383d4c4406980d57e4aae6bbef33586da9dab0
Olha-PSM <Olha84pismennaa@gmail.com>
2024-01-09 21:05:06 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,16 +6,17 @@ import Notiflix from 'notiflix';
 import { nanoid } from 'nanoid';
 
 const getIntialContacts = () => {
-  const saveContacts = localStorage.getItem('contact-item');
+  const saveContacts = localStorage.getItem('contacts');
   if (saveContacts !== null) {
-    return JSON.parse(saveContacts);
+    return (
+      JSON.parse(saveContacts) ?? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+    );
   }
-  return [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ];
 };
 
 export const App = () => {
