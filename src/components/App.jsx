abb8470318e7d940b2ead8ba78bd0231691bc608
Olha-PSM <Olha84pismennaa@gmail.com>
2024-01-09 21:12:07 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,23 +5,18 @@ import { ContactList } from './ContactList/ContactList';
 import Notiflix from 'notiflix';
 import { nanoid } from 'nanoid';
 
-const getIntialContacts = () => {
-  const saveContacts = localStorage.getItem('contacts');
-  if (saveContacts !== null) {
-    return (
-      JSON.parse(saveContacts) ?? [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ]
-    );
-  }
-};
+const initialState = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
 export const App = () => {
-  const [contacts, setContacts] = useState(getIntialContacts);
   const [filter, setFilter] = useState('');
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(localStorage.getItem('contacts')) ?? initialState;
+  });
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
